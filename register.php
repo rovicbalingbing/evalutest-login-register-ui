980e295b30fdacef10fe5af37ba2b03b2154c7c6
John Rovie R. Balingbing <rvc.blngbng@gmail.com>
2023-05-04 15:18:56 +0800

    Uploaded files

---
+++ register.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>EvaluTest | Register</title>
+        
+        <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
+        <link rel="stylesheet" href="assets/css/login.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    </head>
+    <body>
+        <section class="bg vh-100">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                            <div class="card-body p-5 text-center">
+                                <form>  
+                                    <h3 class="heading mb-5">Register Form</h3>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="text" name="name" id="typeNameX-2" class="form-control form-control-lg" placeholder="Name" />
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="text" name="username" id="typeUsernameX-2" class="form-control form-control-lg" placeholder="Username" />
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="email" name="email" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Email Address" />
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Password" />
+                                    </div>
+
+                                    <button class="btn btn-lg btn-block" type="submit" style="background-color: #F89F5B; border-radius: 100px;">Submit</button>
+
+                                </form>
+                                <p class="go-to">Already have an account? <a href="login.php">Login</a> here!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </body>
+</html>
